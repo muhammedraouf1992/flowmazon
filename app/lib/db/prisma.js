a8e495ab7e6c49ac11cf9790fc07e9e8e651745c
muhammed <muhammedraouf92@gmail.com>
2023-12-21 13:18:31 +0200

    fixing add to cart errors

--- app/lib/db/prisma.js
+++ app/lib/db/prisma.js
@@ -1,11 +1,26 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
+
+// const prismaClientSingleton = () => {
+//   return new PrismaClient();
+// };
+
+// const prisma = globalThis.prisma ?? prismaClientSingleton();
+
+// export default prisma;
+
+// if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+const { PrismaClient } = require("@prisma/client");
 
 const prismaClientSingleton = () => {
   return new PrismaClient();
 };
 
-const prisma = globalThis.prisma ?? prismaClientSingleton();
+globalThis.prisma = globalThis.prisma || prismaClientSingleton();
+
+const prisma = globalThis.prisma;
+
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = prisma;
+}
 
 export default prisma;
-
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
